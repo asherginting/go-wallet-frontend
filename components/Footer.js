aaeb0112e10f754fee539f1139220eb7bf7636d3
asherginting <asherginting3@gmail.com>
2022-04-23 13:47:46 +0700

    chore: add logo footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import style from './styles/Footer.module.scss'
 import Link from "next/link";
+import Image from "next/image";
 
 const Footer = ({full}) => {
   return (
@@ -8,17 +9,17 @@ const Footer = ({full}) => {
         {full 
         ? <><div className={style.toptext}>
           <Link href='/'>
-          <h2 className='my-5'>Zwallet</h2>
+          <Image src='/img/logo.png' alt='logo' layout="fixed" width={110} height={100}/>
           </Link>
-          <p className="">Simplify financial needs and saving much time in banking needs with one single app.</p>
+          <p className="">Go - Wallet is simplify financial needs and saving much time in banking needs with one single app.</p>
         </div>
         <div className="my-5 border border-bottom" /> </>
         : <></>}
         <div className='row d-flex flex-row justify-content-between'>
-          <div className='col-12 col-lg-6'>2022 Zwallet. All right reserved.</div>
+          <div className='col-12 col-lg-6'>2022 Go - Wallet. All right reserved.</div>
           <div className='col-12 col-lg-6 mt-4 mt-lg-0 text-end'>
-            <span className='me-5'>+62 5637 8882 9901</span>
-            <span>contact@zwallet.com</span>
+            <span className='me-5'>+62 812 3456 789</span>
+            <span>contact@gowallet.com</span>
           </div>
         </div>
       </div>
